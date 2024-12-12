61368b88cb5dfff2029e14d8e1fcb7d1a3b0e4d0
Subham043 <sahasubham043@gmail.com>
2024-12-12 18:14:49 +0530

    changed the url

--- frontend/src/utils/config/env.ts
+++ frontend/src/utils/config/env.ts
@@ -2,9 +2,9 @@ export const env = {
   API_ENDPOINT:
     import.meta.env.VITE_USER_NODE_ENV === "development"
       ? "http://localhost:8000"
-      : "https://klwb-api.digisole.in",
+      : "https://labourwelfarebackend.digisole.in",
   MAIN_URL:
     import.meta.env.VITE_USER_NODE_ENV === "development"
       ? "http://localhost:5173"
-      : "https://klwb-client.digisole.in",
+      : "https://labourwelfarefrontend.digisole.in",
 } as const;
