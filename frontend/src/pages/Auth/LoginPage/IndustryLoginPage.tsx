ec98acf559df1f6377699f94b65da174cfbd72a1
Subham043 <sahasubham043@gmail.com>
2026-01-14 00:02:46 +0530

    changes done

--- frontend/src/pages/Auth/LoginPage/IndustryLoginPage.tsx
+++ frontend/src/pages/Auth/LoginPage/IndustryLoginPage.tsx
@@ -4,37 +4,42 @@ import { Divider } from "rsuite";
 import Login from "../../../components/Login";
 import { page_routes } from "../../../utils/routes/pages";
 import { api_routes } from "../../../utils/routes/api";
+import { Link } from "react-router-dom";
+import DetailIcon from "@rsuite/icons/Detail";
 
 function IndustryLoginPage() {
   return (
     <div className="row justify-center">
       <div className={classes.info_col}>
-        <IntroScreen />
+        <IntroScreen>
+          <p>
+            <b>
+              If You Don't Have an Account?{" "}
+              <Link to={page_routes.industry.auth.register}>Register</Link>
+            </b>
+          </p>
+          <p>For Industry Registration/Contribution Manual Kindly</p>
+          <p>
+            <b>
+              <a href="/contribution-guidelines.pdf" download={true}>
+                <DetailIcon style={{ fontSize: "1.6rem" }} />
+              </a>
+            </b>
+          </p>
+        </IntroScreen>
         <Divider>Note</Divider>
         <p>
           <b>
-            Industries registering for the Scholarship scheme must ensure the
-            following points
+            Industries must ensure the following points for paying the
+            contribution
           </b>
         </p>
         <ol>
-          <li>
-            The Industry must be registered on the portal before any student
-            (child of employee) can apply for the Scholarship.
-          </li>
-          <li>
-            The Educational Institution where the employee's child is studying
-            must also be registered on the portal.
-          </li>
-          <li>
-            Only after both the Industry and the Educational Institution are
-            successfully registered, the student will be able to REGISTER and
-            apply for the Scholarship.
-          </li>
+          <li>The Industry must be registered on the portal.</li>
           <li>
             The Industry must pay the required contribution to the Labour
-            Welfare Board; only after the contribution is paid will the
-            student's Scholarship application be accepted and processed.
+            Welfare Board before 15th January, 2026 to avoid interest on the
+            contribution.
           </li>
         </ol>
       </div>
